4ad6c98bd913c302d89a0b2059072a956a9cc9cb
Umair Shaheen <109942@eesd.org>
2021-03-12 00:15:46 +0000

    Initial commit

---
+++ Coding File/README.md
@@ -0,0 +1,21 @@
+## Fast Math
+
+For this test you will use everything we've been learning to create a multiplication game. 
+
+How the game works: 
+1. The computer will first introduce the game and its rules (Yes)
+2. The computer will ask if the user is ready to start (Yes)
+3. The computer will choose 2 random numbers between 0-12 (Yes)
+4. The computer will ask the user the multiplication question and start the timer (Kinda)
+5. After the user answers, the computer will let them know how many seconds has elapsed (No)
+6. If the user answers correctly, the computer will ask another multiplication question and restart the timer (Kinda)
+7. The computer will keep track of the total score (No)
+
+### Tips: What you will need 
+
+random 
+time 
+math 
+conditionals 
+while loops
+input
---
+++ Coding File/main.py
@@ -0,0 +1,26 @@
+from random import randint
+
+print ("Welcome to Umair's Game! \nHere's how you are going to play the game: \nYou get 3 seconds to answer the question. If you don't answer in 3 seconds, you'll fail so be careful!")
+
+question = str((output("Are you ready to start? (y/n)"))
+
+play = 1
+while play == 1:
+    x = randint(1,12)
+    y = randint(1,12)
+
+    print ("The multiplication problem is:", x , "*" , y)
+    answer = str(input("What is your answer?"))
+
+    if answer == x*y:
+        print ("That is correct.")
+    else:
+        print ("That is not correct. The correct answer was " + x*y)
+
+score = score + 1
+print ("Your final score is..." + score + "\nNice Job!")
+
+play = int(input("Enter  1 to play again or 0 to exit. "))
+
+ #else:
+    #print ("Thanks for playing my game!") 
